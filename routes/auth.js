693a020841cfea1be2dd6dbcc70861a412fcce9f
rii98 <136151503+rii98@users.noreply.github.com>
2023-12-27 22:50:43 +0545

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,19 @@
 const express = require("express");
-
+const jwt = require("jsonwebtoken");
 const { login, signup } = require("../controller/auth");
 const router = express.Router();
 router.post("/login", login);
 router.post("/signup", signup);
+router.get("/validate", (req, res) => {
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).json({ verified: false });
+  const token = authorization.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
+    return res.json({ verified: true });
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ verified: false });
+  }
+});
 module.exports = router;
